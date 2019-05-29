902abcad9b6d7cd075efc9806937413293f46c26
q55mehqs <eviyuyu14@gmail.com>
2019-05-29 19:38:17 +0900

    実行後にユーザーが確認を行えるように変更

--- Windows_PowerShell/Setting.ps1
+++ Windows_PowerShell/Setting.ps1
@@ -1,8 +1,15 @@
 ﻿$server = Read-Host 'プロキシサーバーを指定してください(http:// も含めてください)'
 $port = Read-Host 'ポート番号を入力してください'
 
-$netsh = 'netsh winhttp set proxy proxy-server="' + $server + ':' + $port + '"\r\n'
+$netsh = 'netsh winhttp set proxy proxy-server="' + $server + ':' + $port + '"'
 $git = 'git config --global http.proxy ' + $server + ':' + $port
 
 $netsh | Out-File ./Start-Proxy.ps1
 Write-Output $git >> ./Start-Proxy.ps1
+
+Write-Output "Write-Output 'プロキシの設定を行いました。'" >> ./Start-Proxy.ps1
+Write-Output "Write-Output '問題が起こった場合は「管理者として実行」を行ってください。'" >> ./Start-Proxy.ps1
+Write-Output "Read-Host 'ボタンを押すとファイル作成プログラムを終了します... '" >> ./Start-Proxy.ps1
+
+Write-Output "プロキシサーバーを介してGitHubの操作を行うファイルを作成しました。"
+Read-Host "ボタンを押すとファイル作成プログラムを終了します... "
--- Windows_PowerShell/Stop-Proxy.ps1
+++ Windows_PowerShell/Stop-Proxy.ps1
@@ -1,2 +1,6 @@
 git config --global --unset http.proxy
 netsh winhttp reset proxy
+
+Write-Output 'プロキシの設定を行いました。'
+Write-Output '問題が起こった場合は「管理者として実行」を行ってください。'
+Read-Host 'ボタンを押すとファイル作成プログラムを終了します... '
