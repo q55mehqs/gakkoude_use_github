8c9ba07590b561d09f0339817fda72e668d44499
q55mehqs <eviyuyu14@gmail.com>
2019-05-29 18:28:47 +0900

    Start-Proxy.ps1 を作成するファイルを作成

---
+++ Windows_PowerShell/Setting.ps1
@@ -0,0 +1,8 @@
+﻿$server = Read-Host 'プロキシサーバーを指定してください(http:// も含めてください)'
+$port = Read-Host 'ポート番号を入力してください'
+
+$netsh = 'netsh winhttp set proxy proxy-server="' + $server + ':' + $port + '"\r\n'
+$git = 'git config --global http.proxy ' + $server + ':' + $port
+
+$netsh | Out-File ./Start-Proxy.ps1
+Write-Output $git >> ./Start-Proxy.ps1
